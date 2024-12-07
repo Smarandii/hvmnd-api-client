0adb648e892d50effcf66b75a0dcc64a17112986
Smarandii <olegsmarandi@gmail.com>
2024-12-07 12:36:30 +0300

    hvmnd-api-client | v2.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hvmnd_api_client',
-    version='1.1.1',
+    version='2.5.0',
     description='Python client library for the Go API application.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
